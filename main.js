2f0368c98a2485da18304723aa20eef7eca9bb47
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-04-08 20:34:25 -0300

    updat proj

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
- function onScroll() {
-  if(scrollY > 0){
-	nav.classList.add('scroll')
-} else{
-    nav.classList.remove('scroll')
-  }
+function onScroll() {
+	if (scrollY > 0) {
+		nav.classList.add('scroll')
+	} else {
+		nav.classList.remove('scroll')
+	}
 }
-function openMenu(){
-  document.body.classList.add('menu-expanded')
+function openMenu() {
+	document.body.classList.add('menu-expanded')
 }
-function closeMenu(){
-  document.body.classList.remove('menu-expanded')
+function closeMenu() {
+	document.body.classList.remove('menu-expanded')
 }
+
+// scrolreveal
+ScrollReveal({
+	origin: 'top',
+	distance: '30px',
+	duration: 1000
+}).reveal(`
+#home, 
+#home img,
+ #home .stats, 
+ #services
+ #services header,
+ #services .card,
+ #about,
+ #about header,
+ #about .content
+ `)
