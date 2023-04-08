0be54c68e2cbd2401c225457a55b59a62fbc87f0
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-04-07 23:23:47 -0300

    updat proj

---
+++ main.js
@@ -0,0 +1,13 @@
+ function onScroll() {
+  if(scrollY > 0){
+	nav.classList.add('scroll')
+} else{
+    nav.classList.remove('scroll')
+  }
+}
+function openMenu(){
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu(){
+  document.body.classList.remove('menu-expanded')
+}
